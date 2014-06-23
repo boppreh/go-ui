888e130c6be2b6387a142d09495440c715b05f21
BoppreH <boppreh@gmail.com>
2014-06-23 16:37:24 -0300

    Add progress function

--- ui.go
+++ ui.go
@@ -2,30 +2,46 @@ package main
 
 import (
 	"github.com/boppreh/forked-go-ui"
+	"time"
 )
 
-func myMain() {
-	w := ui.NewWindow("Test Window", 400, 250)
-	ui.AppQuit = w.Closing
-	w.Open(ui.Layout(ui.NewLabel("Task Description"),
-		ui.NewProgressBar(),
-		nil,
-		ui.Layout(ui.NewButton("Button 1"),
-			ui.NewLineEdit("Edit"),
-			nil,
-			ui.Layout(ui.NewButton("Button 2"),
-				ui.NewButton("Button 3"),
-				ui.NewButton("Button 4")),
-			ui.Layout(ui.NewButton("Button 5"),
-				nil,
-				ui.NewButton("Button 6")))))
+func Progress(title string, descriptionChannel chan string, progressChannel chan int) {
+	w := ui.NewWindow(title, 400, 120)
+
+	descriptionLabel := ui.NewLabel("")
+	progressBar := ui.NewProgressBar()
+	w.Open(ui.Layout(descriptionLabel, progressBar))
 
 	for {
 		select {
+		case d, ok := <-descriptionChannel:
+			if !ok {
+				return
+			}
+			descriptionLabel.SetText(d)
+		case p, ok := <-progressChannel:
+			if !ok {
+				return
+			}
+			progressBar.SetProgress(p)
 		case <-w.Closing:
 			return
 		}
 	}
+}
+
+func myMain() {
+	descriptionChannel := make(chan string)
+	progressChannel := make(chan int)
+	go func() {
+		for i := 1; i <= 100; i++ {
+			progressChannel <- i
+			descriptionChannel <- "Doing " + string(i)
+			time.Sleep(time.Millisecond * 100)
+		}
+		close(progressChannel)
+	}()
+	Progress("Title", descriptionChannel, progressChannel)
 }
 
 func main() {
